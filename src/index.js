09be2049e73bf72a3d02c6109e4b6c225326205e
leotinoco7 <leofptinoco7@gmail.com>
2022-05-07 22:01:19 -0300

    feat: auth, users, jwt, encrypt, fix: routes, controllers, services, index

---
+++ src/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+const app = express();
+const characters = require('./characters/characters.routes');
+const authRoute = require('./auth/auth.route');
+const userRoute = require('./users/users.route.js');
+const connectToDatabase = require('./database/database');
+
+app.use(cors());
+app.use(express.json());
+
+connectToDatabase();
+
+app.get('/', (req, res) => {
+  res.send({ message: 'Hello, world!' });
+});
+
+app.use('/characters', characters);
+app.use('/auth', authRoute);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
